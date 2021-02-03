45ec5b98c8a4132579841db5ddeac021c5e418ba
Renato Diniz <dinizre@hotmail.com>
2021-02-03 10:39:56 -0300

    Api.CrossCutting - Injeção de Dependências

--- src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
+++ src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
@@ -1,6 +1,9 @@
+using System;
 using Api.Data.Context;
+using Api.Data.Implementations;
 using Api.Data.Repository;
 using Api.Domain.Interfaces;
+using Api.Domain.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -11,10 +14,16 @@ namespace Api.CrossCutting.DependencyInjection
         public static void ConfigureDependenciesRepository(IServiceCollection serviceCollection)
         {
             serviceCollection.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+            serviceCollection.AddScoped<IUserRepository, UserImplementation>();
 
             serviceCollection.AddDbContext<MyContext>(
                 options => options.UseMySql("Server=localhost;Port=3306;Database=dbApi;Uid=root;Pwd=admin")
             );
+        }
+
+        private static Type UserImplementation<T>()
+        {
+            throw new NotImplementedException();
         }
     }
 }
