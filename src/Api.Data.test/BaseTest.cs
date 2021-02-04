de83082b5b9ff5af091d1a6dc825d0a22d73407e
Renato Diniz <dinizre@hotmail.com>
2021-02-04 20:25:44 -0300

    Api.Data.Test - Teste Unitarios com xUnit

---
+++ src/Api.Data.test/BaseTest.cs
@@ -0,0 +1,46 @@
+using System;
+using Api.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Api.Data.Test
+{
+    public abstract class BaseTest
+    {
+        public BaseTest()
+        {
+
+        }
+    }
+
+    public class DbTeste : IDisposable
+    {
+        public string dataBaseName = $"dbApiTest_{Guid.NewGuid().ToString().Replace("-", string.Empty)}";
+        public ServiceProvider ServiceProvider { get; private set; }
+
+        public DbTeste()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddDbContext<MyContext>(o =>
+                // o.UseSqlServer($"Persist Security Info=True;Server=localhost;Database={dataBaseName};User=sa;Password=pwd!0407@"),
+                o.UseSqlServer($"Persist Security Info=True;Server=.\\SQLEXPRESS2019;Database={dataBaseName};User=sa;Password=pwd!0407@"),
+                ServiceLifetime.Transient
+            );
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+            using (var context = ServiceProvider.GetService<MyContext>())
+            {
+                context.Database.EnsureCreated();
+            }
+        }
+
+        public void Dispose()
+        {
+            using (var context = ServiceProvider.GetService<MyContext>())
+            {
+                context.Database.EnsureDeleted();
+            }
+        }
+
+    }
+}
--- src/Api.Data/Context/ContextFactory.cs
+++ src/Api.Data/Context/ContextFactory.cs
@@ -7,9 +7,11 @@ namespace Api.Data.Context
     {
         public MyContext CreateDbContext(string[] args)
         {
-            var connectionString = "Server=localhost;Port=3306;Database=dbApi;Uid=root;Pwd=admin";
+            // var connectionString = "Server=localhost;Port=3306;Database=dbApi;Uid=root;Pwd=admin";
+            // optionsBuilder.UseMySql(connectionString); 
+            var connectionString = "Server=.\\SQLEXPRESS2019;Initial Catalog=dbapi;MultipleActiveResultSets=true;User ID=sa;Password=pwd!0407@";
             var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
-            optionsBuilder.UseMySql(connectionString);
+            optionsBuilder.UseSqlServer(connectionString);
             return new MyContext(optionsBuilder.Options);
 
         }
